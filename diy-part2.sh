8c3539ddcd14796a9eeece728b629db1c171a3d9
fndsz <45010248+fndsz@users.noreply.github.com>
2026-04-24 17:56:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.28.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/10.28.1.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名称
 sed -i 's/ImmortalWrt/RAX3KM/g' package/base-files/files/bin/config_generate
