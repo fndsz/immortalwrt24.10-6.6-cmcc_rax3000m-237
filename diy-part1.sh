d8297a19e6dfa481caaa06b02e66703591cf37d1
fndsz <45010248+fndsz@users.noreply.github.com>
2026-04-25 08:47:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,5 @@
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/siwind/luci-app-wolplus.git package/wolplus
+git clone https://github.com/siwind/luci-app-wolplus.git package/luci-app-taiscale
 
