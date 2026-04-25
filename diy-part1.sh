4937d9e83fcc141874a46e61cda2da4e68837661
fndsz <45010248+fndsz@users.noreply.github.com>
2026-04-26 03:25:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci https://github.com/openwrt/luci' >>feed.conf.default
+#echo 'src-git luci https://github.com/openwrt/luci' >>feed.conf.default
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/siwind/luci-app-wolplus.git package/wolplus
+git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/luci-app-tailscale-community
