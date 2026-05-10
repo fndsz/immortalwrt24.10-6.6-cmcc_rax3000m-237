a550be381ebd442fd94d04e376a1fcb1cb4544b1
fndsz <45010248+fndsz@users.noreply.github.com>
2026-05-10 20:27:19 +0800

    Initialize fanchmwr.sh with shebang and package clones
    Added a bash shebang and license information to the script, along with multiple git clone commands for various packages.

--- fanchmwr.sh
+++ fanchmwr.sh
@@ -1,1 +1,23 @@
-123
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git luci https://github.com/openwrt/luci' >>feed.conf.default
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+git clone https://github.com/siwind/luci-app-wolplus.git package/wolplus
+git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/luci-app-tailscale-community
