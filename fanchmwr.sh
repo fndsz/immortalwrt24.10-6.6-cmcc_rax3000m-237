4529f7cb93d3f6e0b949f50f7897962e2a06cc6c
fndsz <45010248+fndsz@users.noreply.github.com>
2026-05-10 20:58:35 +0800

    Update fanchmwr.sh

--- fanchmwr.sh
+++ fanchmwr.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git luci https://github.com/openwrt/luci' >>feed.conf.default
-#echo 'src-git fanchmwrt https://github.com/fanchmwrt/fanchmwrt-packages.git' >>feeds.conf.default
+echo 'src-git fanchmwrt https://github.com/fanchmwrt/fanchmwrt-packages.git' >>feeds.conf.default
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/siwind/luci-app-wolplus.git package/wolplus
